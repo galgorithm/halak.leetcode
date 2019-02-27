8a3df50fa18ebbd5786a649397b7930dc7a1697e
HALAK <halak@sinsiroad.com>
2019-02-27 22:06:01 +0900

    Solve 771, 867

---
+++ LeetCode/0771NumJewelsInStones.cs
@@ -0,0 +1,36 @@
+﻿partial class Solution
+{
+    public int NumJewelsInStones(string J, string S)
+    {
+        var jewelMask = 0L;
+        for (var i = 0; i < J.Length; i++)
+            jewelMask |= 1L << ToIndex(J[i]);
+        
+        var count = 0;
+        for (var i = 0; i < S.Length; i++)
+        {
+            if ((jewelMask & (1L << ToIndex(S[i]))) != 0)
+                count++;
+        }
+
+        return count;
+        
+        int ToIndex(char c) 
+        {
+            if ('A' <= c && c <= 'Z')
+                return c - 'A';
+            else if ('a' <= c && c <= 'z')
+                return c - 'a' + 26;
+            else
+                return c - '0' + 52;
+        }
+    }
+}
+
+partial class Tests
+{
+    [NUnit.Framework.Test(Description = "https://leetcode.com/problems/jewels-and-stones/")]
+    [NUnit.Framework.TestCase("aA", "aAAbbbb", ExpectedResult = 3)]
+    [NUnit.Framework.TestCase("z", "ZZ", ExpectedResult = 0)]
+    public object NumJewelsInStones(string J, string S) => InvokeTest();
+}
---
+++ LeetCode/0867Transpose.cs
@@ -0,0 +1,52 @@
+﻿partial class Solution
+{
+    public int[][] Transpose(int[][] A)
+    {
+        var matrix = new int[A[0].Length][];
+        
+        for (var row = 0; row < matrix.Length; row++)
+        {
+            matrix[row] = new int[A.Length];
+            for (var column = 0; column < A.Length; column++)
+                matrix[row][column] = A[column][row];
+        }
+
+        return matrix;
+    }
+}
+
+partial class Tests
+{
+    [NUnit.Framework.Test(Description = "https://leetcode.com/problems/transpose-matrix/")]
+    [NUnit.Framework.TestCaseSource(nameof(TransposeArgs))]
+    public object Transpose(int[][] A) => InvokeTest();
+
+    static System.Collections.IEnumerable TransposeArgs
+    {
+        get
+        {
+            yield return new NUnit.Framework.TestCaseData((object)new int[][] 
+            {
+                new[] {1, 2, 3},
+                new[] {4, 5, 6},
+                new[] {7, 8, 9},
+            }).Returns(new int[][]
+            {
+                new[] {1, 4, 7},
+                new[] {2, 5, 8},
+                new[] {3, 6, 9},
+            });
+
+            yield return new NUnit.Framework.TestCaseData((object)new int[][] 
+            {
+                new[] {1, 2, 3},
+                new[] {4, 5, 6},
+            }).Returns(new int[][]
+            {
+                new[] {1, 4},
+                new[] {2, 5},
+                new[] {3, 6},
+            });
+        }
+    }
+}
