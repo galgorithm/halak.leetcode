31995ae58d959a7f0c5dd6a47191df31e6f59062
HALAK SHIN <halakshin@gmail.com>
2019-03-02 17:20:27 +0900

    Work in progress 134

---
+++ Alvis.LeetCode/0134GasStation.cs
@@ -0,0 +1,15 @@
+﻿partial class Solution
+{
+    public int CanCompleteCircuit(int[] gas, int[] cost)
+    {
+        return 0;
+    }
+}
+
+partial class Tests
+{
+    [NUnit.Framework.Test(Description = "https://leetcode.com/problems/gas-station/")]
+    [NUnit.Framework.TestCase("[1,2,3,4,5]", "[3,4,5,1,2]", ExpectedResult = 3)]
+    [NUnit.Framework.TestCase("[2,3,4]", "[3,4,3]", ExpectedResult = -1)]
+    public object CanCompleteCircuit(params object[] args) => InvokeTest();
+}
